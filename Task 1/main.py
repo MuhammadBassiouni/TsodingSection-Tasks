051e3165379193179d4c369921c3dc0cb9296131
mohammadbasyouni <mohmah345@gmail.com>
2021-10-26 21:37:48 +0200

    boilerblate

---
+++ Task 1/main.py
@@ -0,0 +1,21 @@
+def cel_to_fehr():
+  pass
+
+
+def add_all_digits(): 
+  pass
+
+
+def interest_for_the_next_month():
+  pass
+
+
+def main():
+  # cel_to_fehr()
+  # add_all_digits()
+  # interest_for_the_next_month()
+  pass
+
+
+if __name__ == "__main__":
+  main()
