e909117df9e60cb4b89243b878998d0c3d41dbb9
Mohammad Basyouni <60100307+mohammadbasyouni@users.noreply.github.com>
2021-10-26 22:45:03 +0200

    q-1

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,5 +1,6 @@
-def cel_to_fehr():
-  pass
+def cel_to_fahr():
+  f = float(input("input fahr-degree: "))
+  print("result:", (5 / 9) * (f - 32))
 
 
 def add_all_digits(): 
@@ -10,12 +11,8 @@ def interest_for_the_next_month():
   pass
 
 
-def main():
-  # cel_to_fehr()
+if __name__ == "__main__":
+  # cel_to_fahr()
   # add_all_digits()
   # interest_for_the_next_month()
   pass
-
-
-if __name__ == "__main__":
-  main()
