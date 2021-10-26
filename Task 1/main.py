9e841610dae922096950fe8b3bb2e42bf2375111
Mohammad Basyouni <60100307+mohammadbasyouni@users.noreply.github.com>
2021-10-26 23:50:45 +0200

    All Done

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,14 +1,24 @@
 def cel_to_fahr():
-  f = float(input("input fahr-degree: "))
-  print("result:", (5 / 9) * (f - 32))
+  fahr = float(input("input fahr-degree: "))
+  print("result:", (5 / 9) * (fahr - 32))
 
 
 def add_all_digits(): 
-  pass
+  digit_str = input("number: ")
+  number = int(digit_str)
+  if not (0 < number < 1000):
+    exit(1)
+  sum = 0
+  for _ in range(len(digit_str)):
+    sum += number % 10
+    number //= 10
+  print(sum)
 
 
 def interest_for_the_next_month():
-  pass
+  balance = float(input("Blanace: "))
+  annual_interest_rate = float(input("Annual Interest Rate : "))
+  print("interest", balance * (annual_interest_rate / 1200))
 
 
 if __name__ == "__main__":
